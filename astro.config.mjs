5f9d0bc3f33720bd109b1364765ae4dfcf9755fe
didntchooseaname <126179594+didntchooseaname@users.noreply.github.com>
2024-08-26 17:40:59 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightThemeRapide from 'starlight-theme-rapide'
-import starlightImageZoom from 'starlight-image-zoom'
-//import starlightImageZoom from 'starlight-showcases'
+import starlightThemeRapide from 'starlight-theme-rapide';
+import starlightImageZoom from 'starlight-image-zoom';
+import mdx from '@astrojs/mdx';
+import embeds from 'astro-embed/integration'
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
+		embeds(), 
+		mdx(),
 		starlight({
 			plugins: [
 				starlightThemeRapide(),
