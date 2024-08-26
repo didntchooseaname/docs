44db7c9f5f37ca80c801d2783b37583333d187dc
didntchooseaname <126179594+didntchooseaname@users.noreply.github.com>
2024-08-26 17:45:18 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,43 +2,43 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide';
 import starlightImageZoom from 'starlight-image-zoom';
-import embeds from 'astro-embed/integration'
+import embeds from 'astro-embed/integration';
 
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [
-				starlightThemeRapide(),
-				starlightImageZoom(),
-			  ],
-			title: 'Docs',
-			tableOfContents: true,
-			defaultLocale: 'root',
-			sidebar: [
-				{ label: 'Home', link: '/' },
-				{
-				  label: 'opsec',
-				  autogenerate: { directory: 'opsec' },
-				},
-				{
-					label: 'pentest',
-					autogenerate: { directory: 'pentest' },
-				  },
-			  ],
-     		locales: {
-				root: {
-					label: '🇬🇧 English',
-					lang: 'en',
-				},
-				fr: {
-					label: '🇫🇷 Français',
-					lang: 'fr',
-				},
-			},
-			social: {
-				github: 'https://github.com/didntchooseaname',
-			},
-		}),
-		embeds(), 
-	],
+  integrations: [
+    starlight({
+      plugins: [
+        starlightThemeRapide(),
+        starlightImageZoom(),
+      ],
+      title: 'Docs',
+      tableOfContents: true,
+      defaultLocale: 'root',
+      sidebar: [
+        { label: 'Home', link: '/' },
+        {
+          label: 'opsec',
+          autogenerate: { directory: 'opsec' },
+        },
+        {
+          label: 'pentest',
+          autogenerate: { directory: 'pentest' },
+        },
+      ],
+      locales: {
+        root: {
+          label: '🇬🇧 English',
+          lang: 'en',
+        },
+        fr: {
+          label: '🇫🇷 Français',
+          lang: 'fr',
+        },
+      },
+      social: {
+        github: 'https://github.com/didntchooseaname',
+      },
+    }),
+    embeds(), 
+  ],
 });
