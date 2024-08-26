d13be1eb4acf2ebe339a402c829b54b4bb735241
didntchooseaname <126179594+didntchooseaname@users.noreply.github.com>
2024-08-26 15:22:14 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,25 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeRapide from 'starlight-theme-rapide'
+import starlightImageZoom from 'starlight-image-zoom'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			plugins: [starlightThemeRapide()],
+			plugins: [starlightThemeRapide()] [starlightImageZoom()],
 			title: 'My Docs',
+			defaultLocale: 'root',
+     		locales: {
+				root: {
+					label: 'English',
+					lang: 'en', // lang is required for root locales
+				},
+				fr: {
+					label: 'Francais',
+				},
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/didntchooseaname',
 			},
 			sidebar: [
 				{
