98119f426164a0f74b1f2b8032d15c77f4def3cd
didntchooseaname <126179594+didntchooseaname@users.noreply.github.com>
2024-08-28 13:52:37 +0200

    tset

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,93 +9,109 @@ import astroD2 from 'astro-d2';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://docs.contactit.fr',
-  integrations: [starlight({
-    credits: false,
-    lastUpdated: true,
-    favicon: './public/contactit.svg',
-    customCss: ['./src/styles/custom.css'],
-    plugins: [starlightThemeRapide(), starlightImageZoom()],
-    title: 'docs.contactit.fr',
-    head: [{
-      tag: 'meta',
-      attrs: {
-        property: 'og:image',
-        content: './public/og.png'
-      }
-    }, {
-      tag: 'meta',
-      attrs: {
-        property: 'twitter:image',
-        content: './public/og.png'
-      }
-    }],
-    logo: {
-      src: './src/assets/contactit.webp'
-    },
-    defaultLocale: 'root',
-    sidebar: [{
-      label: 'Certifications',
-      translations: {
-        'fr': 'Certifications'
+  integrations: [
+    starlight({
+      credits: false,
+      lastUpdated: true,
+      favicon: './public/contactit.svg',
+      customCss: ['./src/styles/custom.css'],
+      plugins: [starlightThemeRapide(), starlightImageZoom()],
+      title: 'docs.contactit.fr',
+      head: [
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'og:image',
+            content: './public/og.png'
+          }
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            property: 'twitter:image',
+            content: './public/og.png'
+          }
+        }
+      ],
+      logo: {
+        src: './src/assets/contactit.webp'
       },
-      collapsed: false,
-      autogenerate: {
-        directory: 'certifications',
-        collapsed: true
-      }
-    }, {
-      label: 'Offensive',
-      translations: {
-        'fr': 'Offensif'
+      defaultLocale: 'root',
+      sidebar: [
+        {
+          label: 'Certifications',
+          translations: {
+            fr: 'Certifications'
+          },
+          collapsed: false,
+          autogenerate: {
+            directory: 'certifications',
+            collapsed: true
+          }
+        },
+        {
+          label: 'Offensive',
+          translations: {
+            fr: 'Offensif'
+          },
+          autogenerate: {
+            directory: 'offensive',
+            collapsed: true
+          }
+        },
+        {
+          label: 'Defensive',
+          translations: {
+            fr: 'Defensif'
+          },
+          collapsed: false,
+          autogenerate: {
+            directory: 'defensive',
+            collapsed: true
+          }
+        },
+        {
+          label: 'Opsec',
+          translations: {
+            fr: 'Opsec'
+          },
+          collapsed: false,
+          autogenerate: {
+            directory: 'opsec',
+            collapsed: true
+          }
+        },
+        {
+          label: 'Misc',
+          translations: {
+            fr: 'Autre'
+          },
+          collapsed: false,
+          autogenerate: {
+            directory: 'misc',
+            collapsed: true
+          }
+        }
+      ],
+      locales: {
+        root: {
+          label: '🇬🇧 English',
+          lang: 'en'
+        },
+        fr: {
+          label: '🇫🇷 Français',
+          lang: 'fr'
+        }
       },
-      autogenerate: {
-        directory: 'offensive',
-        collapsed: true
+      social: {
+        github: 'https://github.com/didntchooseaname',
+        linkedin: 'https://www.linkedin.com/in/didntchooseaname'
       }
-    }, {
-      label: 'Defensive',
-      translations: {
-        'fr': 'Defensif'
-      },
-      collapsed: false,
-      autogenerate: {
-        directory: 'defensive',
-        collapsed: true
-      }
-    }, {
-      label: 'Opsec',
-      translations: {
-        'fr': 'Opsec'
-      },
-      collapsed: false,
-      autogenerate: {
-        directory: 'opsec',
-        collapsed: true
-      }
-    }, {
-      label: 'Misc',
-      translations: {
-        'fr': 'Autre'
-      },
-      collapsed: false,
-      autogenerate: {
-        directory: 'misc',
-        collapsed: true
-      }
-    }],
-    locales: {
-      root: {
-        label: '🇬🇧 English',
-        lang: 'en'
-      },
-      fr: {
-        label: '🇫🇷 Français',
-        lang: 'fr'
-      }
-    },
-    social: {
-      github: 'https://github.com/didntchooseaname',
-      linkedin: 'https://www.linkedin.com/in/didntchooseaname'
-    }
-  }), embeds(), mdx(), astroD2()]
+    }),
+    embeds(),
+    mdx(),
+    astroD2({
+      skipGeneration: true
+    })
+  ]
 });
